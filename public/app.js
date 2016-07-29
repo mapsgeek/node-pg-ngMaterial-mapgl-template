24239f5acb282c5713b8281bc13e72645d530ab4
Daniel Baah <dbaah@spatialdev.com>
2016-07-29 14:46:13 -0700

    strip our server for gh-pages

--- public/app.js
+++ public/app.js
@@ -7,13 +7,13 @@ angular.module('myApp', [
 ]).
     config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, jwtInterceptorProvider) {
 
-        $urlRouterProvider.otherwise("/login");
+        $urlRouterProvider.otherwise("/map/@47,-122,2");
 
         $stateProvider
             .state('map', {
                 url: "/map/@{lat},{lng},{zoom}?left-panel&right-panel",
                 //url: "/map",
-                templateUrl: "app/templates/map.html",
+                templateUrl: "node-pg-ngMaterial-mapgl-template/public/app/templates/map.html",
                 //resolve: {
                 //    // inject user into map before controller loads
                 //    user: function (userService) {
@@ -27,29 +27,29 @@ angular.module('myApp', [
             })
             .state('login', {
                 url: "/login",
-                templateUrl: "app/templates/login.html",
+                templateUrl: "node-pg-ngMaterial-mapgl-template/public/app/templates/login.html",
                 controller: 'LoginCtrl'
             });
 
         // use the HTML5 History API to remove the #hash in the URL
         $locationProvider.html5Mode(true);
 
-        jwtInterceptorProvider.tokenGetter = function() {
-            return localStorage.getItem('id_token');
-        };
+        //jwtInterceptorProvider.tokenGetter = function() {
+        //    return localStorage.getItem('id_token');
+        //};
 
-        $httpProvider.interceptors.push(function ($q, $location,jwtHelper) {
-            return {
-                response: function (response) {
-                    // do something on success
-                    $location.url('/map/@14.1394,32.9774,1.96');
-                    return response;
-                }, responseError: function (response) {
-                    if (response.status === 304) $location.url('/login');
-                    return $q.reject(response);
-                }
-            };
-        });
+        //$httpProvider.interceptors.push(function ($q, $location,jwtHelper) {
+        //    return {
+        //        response: function (response) {
+        //            // do something on success
+        //            $location.url('/map/@14.1394,32.9774,1.96');
+        //            return response;
+        //        }, responseError: function (response) {
+        //            if (response.status === 304) $location.url('/login');
+        //            return $q.reject(response);
+        //        }
+        //    };
+        //});
     }).run(function($q, userService, jwtHelper, $rootScope){
 
     })
